9c4c4341cd3aa7dc8e3c5c3a545b181c6f566998
Pratibha Upadhyay <75491789+pratibha-0309@users.noreply.github.com>
2020-12-04 11:15:00 -0500

    Add files via upload

---
+++ reaminder.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    while True:
+        notification.notify( title=" PLEASE DRINK WATER",message="A normal adult can excrete up to 20 liters per day"" and does"" not ""exceed three to four cups of water in an hour, because that's what a normal kidney can excrete at a time."
+
+                         .format(app_icon="E:\icon.ico"),
+                         timeout=3
+    )
+        time.sleep(6)
+#time.sleep(60*60)
